bae708af58d0b0372b887825a8eee1350bf8a60c
slarmoo <matthew.s.medford@gmail.com>
2025-02-25 19:38:31 -0700

    Got it working! Now to start with the rest of the project

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -6,8 +6,9 @@ public class Main {
     public static void main(String[] args) {
         ChessPiece piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
-        Service service = new UserService();
-        Server server = new Server(service);
-        server.run(8080);
+//        Service service = new UserService();
+        Server server = new Server();
+        server.run(8081);
+        System.out.println(server.port());
     }
 }
