7d3792893dac4fbb80e5dda4775259cc3ca7ea04
slarmoo <matthew.s.medford@gmail.com>
2025-03-12 17:43:22 -0600

    Table initiation

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,13 +1,10 @@
 import chess.*;
-import dataaccess.DatabaseManager;
 import server.Server;
-import service.*;
 
 public class Main {
     public static void main(String[] args) {
         ChessPiece piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
-//        Service service = new UserService();
         Server server = new Server();
         server.run(8081);
         System.out.println(server.port());
--- server/src/main/java/dataaccess/SQLDAO.java
+++ server/src/main/java/dataaccess/SQLDAO.java
@@ -1,12 +1,58 @@
 package dataaccess;
 
+import com.google.gson.Gson;
+import exception.ResponseException;
+import model.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.sql.*;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
 
 public class SQLDAO {
+
+    private static final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  auth (
+            `username` varchar(20) NOT NULL,
+            `authToken` varChar(63) NOT NULL
+            )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS  user (
+            `username` varchar(20) NOT NULL,
+            `password` varChar(63) NOT NULL,
+            `email` varChar(63) NOT NULL
+            )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS  game (
+            `id` int NOT NULL AUTO_INCREMENT,
+            `whiteUsername` varchar(20),
+            `blackUsername` varchar(20),
+            `gameName` varchar(20) NOT NULL,
+            `chessGame` text NOT NULL,
+            PRIMARY KEY (`id`)
+            )
+            """
+    };
+
     static {
-        try {
+        try (var conn = DatabaseManager.getConnection()) {
             DatabaseManager.createDatabase();
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
         } catch (Exception e) {
             System.out.println(e);
+//            throw new DataAccessException(String.format("Unable to configure database: %s", e.getMessage()));
         }
     }
+
+//    private void
+
 }
