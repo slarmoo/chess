c33a3e2fb5f3c33066b89d2330ae2647bff110aa
slarmoo <matthew.s.medford@gmail.com>
2025-03-25 22:53:00 -0600

    chessboard ui finalized

--- client/src/main/java/ui/ChessBoardUI.java
+++ client/src/main/java/ui/ChessBoardUI.java
@@ -7,27 +7,42 @@ public class ChessBoardUI {
     private static final String whiteBGColor = EscapeSequences.SET_BG_COLOR_LIGHT_GREY;
     private static final String blackPieceColor = EscapeSequences.SET_TEXT_COLOR_BLACK;
     private static final String whitePieceColor = EscapeSequences.SET_TEXT_COLOR_WHITE;
+    private static final String edgeBGColor = EscapeSequences.SET_BG_COLOR_DARK_GREY;
+    private static final String edgeTextColor = EscapeSequences.SET_TEXT_COLOR_WHITE;
+    private static final String[] edgeLetters = {"h", "g", "f", "e", "d", "c", "b", "a"};
 
-    public static void printBoard(ChessBoard board, boolean isUpsidedown) {
-        final int boardStart = isUpsidedown ? 8 : 1;
-        final int boardEnd = isUpsidedown ? 1 : 8;
-        if(isUpsidedown) {
+    public static void printBoard(ChessBoard board, boolean isRightSideUp) {
+        printULBorder(isRightSideUp);
+        if(isRightSideUp) {
             for(int i = 8; i >= 1; i--) {
+                System.out.print(edgeBGColor);
+                System.out.print(edgeTextColor);
+                System.out.print(getPrettyCharacter(i + ""));
                 for(int j = 8; j >= 1; j--) {
                     printPiece(i, j, board.getPiece(new ChessPosition(i, j)));
                 }
+                System.out.print(edgeBGColor);
+                System.out.print(edgeTextColor);
+                System.out.print(getPrettyCharacter(i + ""));
                 System.out.print(EscapeSequences.RESET_BG_COLOR);
                 System.out.print("\n");
             }
         } else {
             for(int i = 1; i <= 8; i++) {
+                System.out.print(edgeBGColor);
+                System.out.print(edgeTextColor);
+                System.out.print(getPrettyCharacter(i + ""));
                 for(int j = 1; j <= 8; j++) {
                     printPiece(i, j, board.getPiece(new ChessPosition(i, j)));
                 }
+                System.out.print(edgeBGColor);
+                System.out.print(edgeTextColor);
+                System.out.print(getPrettyCharacter(i + ""));
                 System.out.print(EscapeSequences.RESET_BG_COLOR);
                 System.out.print("\n");
             }
         }
+        printULBorder(isRightSideUp);
     }
 
     private static void printPiece(int i, int j, ChessPiece piece) {
@@ -57,5 +72,27 @@ public class ChessBoardUI {
             case PAWN -> EscapeSequences.WHITE_PAWN;
             case null -> EscapeSequences.EMPTY;
         };
+    }
+
+    private static void printULBorder(boolean isRightSideUp) {
+        System.out.print(edgeBGColor);
+        System.out.print(edgeTextColor);
+        System.out.print(getPrettyCharacter(" "));
+        if(isRightSideUp) {
+            for(int j = 7; j >= 0; j--) {
+                System.out.print(getPrettyCharacter(edgeLetters[j]));
+            }
+        } else {
+            for(int j = 0; j <= 7; j++) {
+                System.out.print(getPrettyCharacter(edgeLetters[j]));
+            }
+        }
+        System.out.print(getPrettyCharacter(" "));
+        System.out.print(EscapeSequences.RESET_BG_COLOR);
+        System.out.print("\n");
+    }
+
+    private static String getPrettyCharacter(String character) {
+        return "\u2003" + character + " ";
     }
 }
